691fc89ca2fe894865f12ab8b2c868a40bc99582
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2024-03-22 17:38:35 +0530

    Drawing feature added

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("myCanvas"),
+toolBtns = document.querySelectorAll(".tool"),
+ctx = canvas.getContext("2d");
+
+//Global variables with default values
+let isDrawing = false;
+brushWidth = 5;
+
+window.addEventListener("load", () => {
+    // Setup the canvas dimensions and properties
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight
+});
+
+const startDraw = () => {
+    isDrawing = true;
+    ctx.beginPath();//New path
+    ctx.lineWidth = brushWidth;
+}
+
+const drawing = (e) => {
+    if(!isDrawing) return;
+    ctx.lineTo(e.offsetX, e.offsetY); // Creating line according to mousepointer
+    ctx.stroke(); //
+}
+
+toolBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector(".options .active").classList.remove("active");
+        btn.classList.add("active");
+        console.log(btn.id);
+    })
+});
+
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", () => isDrawing = false);
